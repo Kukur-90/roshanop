c5616593cf9894ff6ce9b8aed934780d3d347108
Kukur-90 <106947220+Kukur-90@users.noreply.github.com>
2022-09-26 23:09:07 +0545

    Create roshanop.py
    pip install mechanize requests bs4
    pkg install git
    git clone https://hgithub.com/Kukur-90/roshanop.git
    cd roshanop
    python roshanop.py

---
+++ roshanop.py
@@ -0,0 +1,22 @@
+import Android,platform
+
+def run():
+
+        elif vau =="32bit":
+
+            __import__("uidcr3k32").Kukur.sir()
+
+        else:
+
+            print(" UNKNOWN DEVICE ! ")
+
+    except:
+
+        print(" SUPPORT ALL DEVICE  ! ")
+
+if __name__ == '__main__':
+
+    os.system("git pull")
+
+    run()
+
